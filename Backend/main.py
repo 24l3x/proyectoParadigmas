ca07da77181563e36f9510be08065621152bdc90
24l3x <2alexander.rivas@gmail.com>
2026-09-18 00:34:54 -0600

    Cambio a postgresql como motor de base de datos y conexion a python

--- Backend/main.py
+++ Backend/main.py
@@ -1,21 +1,13 @@
 from fastapi import FastAPI
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
-
-# 1. Credenciales de la Base de Datos
-# Formato: mysql+pymysql://usuario:contraseña@host:puerto/base_de_datos
+# Formato: postgresql://usuario:contraseña@host:puerto/base_de_datos
 # El host es 'db' porque así se llama el servicio en el docker-compose
-DATABASE_URL = "mysql+pymysql://user:host@db:3306/Hospital"
-
-# 2. Configuración de SQLAlchemy
+DATABASE_URL = "postgresql://user:host@db:5432/Hospital"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
-
-# 3. Inicializamos FastAPI
 app = FastAPI()
-
-# 4. Creamos nuestra primera ruta de prueba
 @app.get("/")
 def ruta_principal():
     return {"mensaje": "¡El servidor de Python y SQLAlchemy están listos!"}
